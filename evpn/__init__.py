de760d4d2c8f8ae99f7a191b325dd773943715c7
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2022-11-17 06:18:59 +0200

    fix: init import

--- evpn/__init__.py
+++ evpn/__init__.py
@@ -4,9 +4,7 @@ _name = platform.system()
 
 if _name == "Windows":
     from .core.WindowsApi import WindowsApi as ExpressVpnApi
-elif _name == "Linux":
-    from .core.LinuxApi import LinuxApi as ExpressVpnApi
-elif _name == "Darwin":
+elif _name in ("Linux", "Darwin"):
     from .core.LinuxApi import LinuxApi as ExpressVpnApi
 else:
     raise NotImplementedError(f"Can't run on {_name} platform.")
